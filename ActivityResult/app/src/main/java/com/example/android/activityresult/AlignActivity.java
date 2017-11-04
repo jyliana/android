57f9884ea7f6a431bf4ef3c471d671c1024783a5
InnaLapina <jyliana@gmail.com>
2017-11-04 20:46:07 +0200

    RequstCode & ResultCode

---
+++ ActivityResult/app/src/main/java/com/example/android/activityresult/AlignActivity.java
@@ -0,0 +1,49 @@
+package com.example.android.activityresult;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+
+public class AlignActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnLeft;
+    Button btnCenter;
+    Button btnRight;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_align);
+
+        btnLeft = findViewById(R.id.btnLeft);
+        btnCenter = findViewById(R.id.btnCenter);
+        btnRight = findViewById(R.id.btnRight);
+
+        btnLeft.setOnClickListener(this);
+        btnCenter.setOnClickListener(this);
+        btnRight.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent();
+
+        switch (view.getId()) {
+            case R.id.btnLeft:
+                intent.putExtra("alignment", Gravity.START);
+                break;
+            case R.id.btnCenter:
+                intent.putExtra("alignment", Gravity.CENTER);
+                break;
+            case R.id.btnRight:
+                intent.putExtra("alignment", Gravity.END);
+                break;
+        }
+        setResult(RESULT_OK, intent);
+        finish();
+
+    }
+}
