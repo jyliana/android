49f0c39e085a6614d50a8d4309df53bfa09048e5
InnaLapina <jyliana@gmail.com>
2017-11-18 14:52:32 +0200

    browser

---
+++ 32SimpleBrowser/app/src/main/java/com/example/android/a32simplebrowser/BrowserActivity.java
@@ -0,0 +1,21 @@
+package com.example.android.a32simplebrowser;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class BrowserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_browser);
+
+        WebView webView = findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient());
+        Uri data = getIntent().getData();
+        webView.loadUrl(data.toString());
+    }
+}
